694e47869f638bde72c8df367e70f81de9334ae8
varat7v2 <varat.7v2@gmail.com>
2020-12-11 20:33:51 +0545

    first commit

---
+++ faceDetection_frozenGraph.py
@@ -0,0 +1,62 @@
+import sys
+import time
+import numpy as np
+import tensorflow as tf
+import cv2
+
+# from utils import label_map_util
+# from utils import visualization_utils_color as vis_util
+
+class TensoflowFaceDector(object):
+    def __init__(self, PATH_TO_CKPT):
+        """Tensorflow detector
+        """
+        self.inference_list = []
+        self.count = 0
+
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+
+        with self.detection_graph.as_default():
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            self.sess = tf.Session(graph=self.detection_graph, config=config)
+            self.windowNotSet = True
+
+
+    def run(self, image):
+        """image: bgr image
+        return (boxes, scores, classes, num_detections)
+        """
+        image_np = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        # Actual detection.
+        start_time = time.time()
+        (boxes, scores, classes, num_detections) = self.sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        elapsed_time = time.time() - start_time
+        self.inference_list.append(elapsed_time)
+        self.count = self.count + 1
+        average_inference = sum(self.inference_list)/self.count
+        # print('Average inference time: {}'.format(average_inference))
+
+        return (boxes, scores, classes, num_detections)
---
+++ myFACENET.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+from facenet import facenet, detect_face
+import os, sys, glob
+import time
+import pickle
+
+# modeldir = './model/facenet-20180408-102900.pb'     #output: 512D vector
+modeldir = './model/facenet-20170511-185253.pb'   #output: 128D vector
+mtcnn_models='./mtcnn-models'
+
+class FACENET_EMBEDDINGS(object):
+    def __init__(self, PATH_TO_CKPT):
+
+        # self.detection_graph = tf.Graph()
+        # with self.detection_graph.as_default():
+        #     od_graph_def = tf.GraphDef()
+        #     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        #         serialized_graph = fid.read()
+        #         od_graph_def.ParseFromString(serialized_graph)
+        #         tf.import_graph_def(od_graph_def, name='')
+
+
+        # with self.detection_graph.as_default():
+        #     config = tf.ConfigProto()
+        #     config.gpu_options.allow_growth = True
+        #     self.sess = tf.Session(graph=self.detection_graph, config=config)
+        #     self.windowNotSet = True
+
+        with tf.Graph().as_default():
+            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+            self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+
+            # with self.sess.as_default():
+            facenet.load_model(PATH_TO_CKPT)
+            self.images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            self.embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            self.phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = self.embeddings.get_shape()[1]
+            self.emb_array = np.zeros((1, embedding_size))
+
+
+    def run(self, faces):
+        t1 = time.time()
+        feed_dict = {self.images_placeholder: faces, self.phase_train_placeholder: False}
+        self.emb_array = self.sess.run(self.embeddings, feed_dict=feed_dict)
+        time_lapse = (time.time() - t1)*1000
+        # print(str(time_lapse)+'milliseconds')
+
+        return self.emb_array
+
+    # with tf.Graph().as_default():
+    #     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    #     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    #     with sess.as_default():
+    #         # print('Loading Modal')
+    #         facenet.load_model(modeldir)
+    #         images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+    #         embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+    #         phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+    #         embedding_size = embeddings.get_shape()[1]
+    #         emb_array = np.zeros((1, embedding_size))
+                
+    #         feed_dict = {images_placeholder: faces, phase_train_placeholder: False}
+    #         emb_array = sess.run(embeddings, feed_dict=feed_dict)
+    
+    # return emb_array
+                    
+
+def main():
+    faces = list()
+    myfacenet = FACENET_EMBEDDINGS(modeldir)
+    for img in glob.glob('cropped_images'+'/*.jpg'):
+        frame = cv2.resize(cv2.imread(img), (160,160), interpolation=cv2.INTER_LINEAR)
+        faces.append(facenet.prewhiten(frame))
+    
+    face_encoding = myfacenet.run(faces)
+    # print(face_encoding)
+
+if __name__ == '__main__':
+    main()
